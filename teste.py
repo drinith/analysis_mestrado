40c417ab82512828315cf31ca56768c5eb3a0e1a
mintsede <mintsede>
2024-02-24 17:17:13 -0300

    refazendo as analises

--- teste.py
+++ teste.py
@@ -6,7 +6,7 @@ from IPython.display import display, HTML
 
 if '__main__'==__name__:
     
-    name = 'smartbugs-curated'
+    name = 'verified-smart-contracts'
     source_solidity = f'./repositories/{name}/'
     
     analise='m'
@@ -39,7 +39,7 @@ if '__main__'==__name__:
 
     ### Mithril
     elif(analise=='m'):
-        mythril_version ='0.23.15'	
+        mythril_version ='0.24.3'	
         dasp_dic = {'Write to Arbitrary Storage Location': 'access_control', 'Integer Overflow and Underflow': 'arithmetic', 'Timestamp Dependence': 'time_manipulation', 'Assert Violation': 'Other', 'Reentrancy': 'reentrancy', 'DoS with Failed': 'denial_service', 'Unprotected Ether Withdrawal': 'access_control', 'Delegatecall to Untrusted Callee': 'Other', 'Authorization through tx.origin': 'access_control', 'Unchecked Call Return Value': 'unchecked_low_calls', 'Weak Sources of Randomness from Chain Attributes': 'bad_randomness', 'Unprotected SELFDESTRUCT Instruction': 'access_control', 'Transaction Order Dependence': 'Other'}
         ma = MythrilAnalysis('0.4.26')
         destiny_analysis = f'./mythril/{mythril_version}_{name}/'
@@ -51,9 +51,9 @@ if '__main__'==__name__:
         print(f'{destiny_analysis}{mythril_version}_slither_{name}')
 
        
-        # ma.run_analysis_diretory(diretory_in=source_solidity,diretory_out=destiny_analysis)
+        ma.run_analysis_diretory(diretory_in=source_solidity,diretory_out=destiny_analysis)
 
-        # ma.resume_json(f'{destiny_analysis}json/',f'{destiny_analysis}json_analysis/')
+        ma.resume_json(f'{destiny_analysis}json/',f'{destiny_analysis}json_analysis/')
 
         df = ma.build_dataframe_from_json(f'{destiny_analysis}json_analysis/',f'{destiny_analysis}results/')
         
