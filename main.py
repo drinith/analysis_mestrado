f44281dc77953129da0b3a247b7baaa6564d0d5e
drinith <drinith>
2024-03-07 17:26:57 -0300

    acertando o main com as novas funções

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import os
 from slither.slither_analysis import SlitherAnalysis
 from mythril.mythril_analysis import MythrilAnalysis
+import pandas as pd
 
 if '__main__'==__name__:
 
-    version='0.8.0'
+    
     name = 'smartbugs-curated'
     source_solidity = f'./repositories/{name}/'
   
     
-    analise='s'
+    analise='m'
 
     if(analise=='s'):
-        
+        version='0.10.0'
         sa = SlitherAnalysis('0.4.26')
         destiny_analysis = f'./slither/{version}_{name}/'
-
+        dasp_dic = {'arbitrary-send': 'access_control', 'assembly': 'Ignore', 'calls-loop': 'denial_service', 'constable-states': 'Ignore', 'constant-function': 'Ignore', 'controlled-delegatecall': 'access_control', 'deprecated-standards': 'Ignore', 'erc20-indexed': 'Ignore', 'erc20-interface': 'Ignore', 'external-function': 'Ignore', 'incorrect-equality': 'Other', 'locked-ether': 'Other', 'low-level-calls': 'unchecked_low_calls', 'naming-convention': 'Ignore', 'reentrancy-benign': 'reentrancy', 'reentrancy-eth': 'reentrancy', 'reentrancy-no-eth': 'reentrancy', 'shadowing-abstract': 'Ignore', 'shadowing-builtin': 'Ignore', 'shadowing-local': 'Ignore', 'shadowing-state': 'Ignore', 'solc-version': 'Ignore', 'suicidal': 'access_control', 'timestamp': 'time_manipulation', 'tx-origin': 'access_control', 'uninitialized-local': 'Other', 'uninitialized-state': 'Other', 'uninitialized-storage': 'Other', 'unused-return': 'unchecked_low_calls', 'unused-state': 'Ignore'}
 
         print(os.getcwd())
 
@@ -32,17 +33,27 @@ if '__main__'==__name__:
 
         # sa.resume_json(f'{destiny_analysis}json/',f'{destiny_analysis}json_analysis/')
 
-        df = sa.montar_dataframe_json(f'{destiny_analysis}json_analysis/',f'{destiny_analysis}results/')
+        df = sa.build_dataframe_from_json(f'{destiny_analysis}json_analysis/',f'{destiny_analysis}results/')
 
-        sa.soma_dataframe(df,f'{destiny_analysis}{version}_slither_{name}')
+        sa.sum_dataframe(df,f'{destiny_analysis}{version}_slither_{name}')
 
         sa.dasp(df,f'{destiny_analysis}{version}_slither_{name}')
+
+        df_dasp = ma.transform_dasp(dasp_dic,df,f'{destiny_analysis}')
+
+       # ma.dasp(df,f'{destiny_analysis}{version}_slither_{name}')
+        
+        df_gabarito = pd.read_excel("helps/gabarito_dataset.xlsx").fillna(0)
+        sa.accuracy(df_dasp,df_gabarito,f'{destiny_analysis}')
 
     ### Mithril
     elif(analise=='m'):
         
+        version='0.24.7'        
         ma = MythrilAnalysis('0.4.26')
         destiny_analysis = f'./mythril/{version}_{name}/'
+        dasp_dic = {'Call data forwarded with delegatecall()': 'access_control', 'DELEGATECALL to a user-supplied address': 'access_control', 'Dependence on predictable environment variable': 'Other', 'Dependence on predictable variable': 'Other', 'Ether send': 'access_control', 'Exception state': 'Other', 'Integer Overflow ': 'arithmetic', 'Integer Underflow': 'arithmetic', 'Message call to external contract': 'reentrancy', 'Multiple Calls': 'Ignore', 'State change after external call': 'reentrancy', 'Transaction order dependence': 'front_running', 'Unchecked CALL return value': 'unchecked_low_calls', 'Unchecked SUICIDE': 'access_control', 'Use of tx.origin': 'access_control'}
+
         print(os.getcwd())
 
 
@@ -58,8 +69,14 @@ if '__main__'==__name__:
 
         ma.resume_json(f'{destiny_analysis}json/',f'{destiny_analysis}json_analysis/')
 
-        df = ma.montar_dataframe_json(f'{destiny_analysis}json_analysis/',f'{destiny_analysis}results/')
+        df = ma.build_dataframe_from_json(f'{destiny_analysis}json_analysis/',f'{destiny_analysis}results/')
 
-        ma.soma_dataframe(df,f'{destiny_analysis}{version}_slither_{name}')
+        ma.sum_dataframe(df,f'{destiny_analysis}{version}_mythril_{name}')
+
+        df_dasp = ma.transform_dasp(dasp_dic,df,f'{destiny_analysis}')
 
        # ma.dasp(df,f'{destiny_analysis}{version}_slither_{name}')
+        
+        df_gabarito = pd.read_excel("helps/gabarito_dataset.xlsx").fillna(0)
+        ma.accuracy(df_dasp,df_gabarito,f'{destiny_analysis}')
+
